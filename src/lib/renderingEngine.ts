d20f9f0d1643d17475860f0a0453ac3bc3c0ba75
Khalamander <alikhank@mcmaster.ca>
2025-03-08 00:49:28 -0500

    fixed missing file

---
+++ src/lib/renderingEngine.ts
@@ -0,0 +1,6 @@
+
+// Re-export rendering functions from the new modular structure
+import { drawTrails } from './rendering/TrailManager';
+import { drawParticles } from './rendering/ParticleRenderer';
+
+export { drawTrails, drawParticles };
